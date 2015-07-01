212ae5b59ac1d7e2554c3aa08e1095f354b71cb4
Andy Wang <andyatcal@berkeley.edu>
2015-07-01 09:30:52 +0800

    a good start of cc

--- halfedge_cc.cpp
+++ halfedge_cc.cpp
@@ -1,20 +1,37 @@
 // Catmull-Clark Subdivision Project
 // Andy Wang, UC Berkeley July 1st 2015
 
-#include <GLUT/glut.h>
+#include <GLUT/GLUT.h>
 #include <math.h>
-#include <vector.h>
-#include <eigen3/Eigen>
+#include <vector>
+#include <Eigen/Dense>
+#include <iostream>
 
 #define PI 3.1415926535897932384626433832795028841971693993751058209
 
 using namespace std;
+using namespace Eigen;
+
+//************************************************************
+//          Viewport class
+//************************************************************
+class Viewport;
+
+class Viewport {
+    public:
+        int width, hight;
+};
 
 //************************************************************
 //          Global Variables
 //************************************************************
 
-// Specify later
+Viewport viewport;
+string USAGE = "USAGE: cc SUBDIVSION_LEVEL";
+//float angle = 0.0; //Initial Rotation Angle
+bool wireframe = false; //Initial state of wireframe
+bool smoothShading = false; //Initial state of smooth shading
+bool lightOn = false; //Initial state of GL_Light
 
 //************************************************************
 //          Geometry Classes
@@ -23,5 +40,115 @@ using namespace std;
 // Mesh class. A Mesh class contains a list of faces, a list of
 // vertices, and a list of edges. It also specifies the connectivity
 // among those geometries.
-class Mesh(){};
+class Mesh;
 
+// Face class. A Face class represents a list of vertices (more than 3)
+// connected by edges.
+class Face;
+
+// Halfedge class. A halfedge represents an edge starting from one vertex
+// and pointing to another vertex.
+class Halfedge;
+
+//************************************************************
+//          OpenGL Display Functions
+//************************************************************
+void render(void);
+
+void reshape(int w, int h);
+
+void keyboard(unsigned char c, int x, int y);
+
+void mouse(int button, int state, int x, int y);
+
+void render(void) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glutSwapBuffers();
+}
+
+void reshape(int w, int h) {
+    if (h==0) {h=1;} // prevent a division by zero when setting aspect ratio
+    glViewport(0,0,w,h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity(); 
+    gluPerspective(45, (float)w/h, 0.1, 100);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void myFrameMove() {
+  glutPostRedisplay();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    if (key == 27) {
+        exit(0);
+    }
+    if (key == 's') {
+        if (smoothShading) {
+            glShadeModel(GL_FLAT);
+            smoothShading = false;
+        } else {
+            glShadeModel(GL_SMOOTH);
+            smoothShading = true;
+        }
+    }
+    if (key == 'w') {
+        if (wireframe) {
+            glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+            wireframe = false;
+        } else {
+            glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+            wireframe = true;
+        }
+    }
+    if (key == 'l') {
+        if (lightOn) {
+            glDisable(GL_LIGHTING);
+            lightOn = false;
+        } else {
+            glEnable(GL_LIGHTING);
+            lightOn = true;
+        }
+    }
+    glutPostRedisplay();
+}
+
+void keySpecial(int key, int x, int y) {
+    bool shiftDown = (glutGetModifiers() == GLUT_ACTIVE_SHIFT);
+    glutPostRedisplay();
+}
+
+void mouse(int button, int state, int x, int y) {
+    if (button == GLUT_RIGHT_BUTTON) {
+        exit(0);
+    }
+}
+
+//************************************************************
+//          Main Function
+//************************************************************
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+    viewport.width = 640;
+    viewport.hight = 480;
+
+    glutInitWindowSize(viewport.width, viewport.hight);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Catmull Clark Subdivision");
+
+
+    glutDisplayFunc(render);
+
+    // General UI functions
+    glutReshapeFunc(reshape);
+    glutIdleFunc(myFrameMove); 
+    glutKeyboardFunc(keyboard);
+    glutSpecialFunc(keySpecial);
+
+    glutMainLoop();
+}
