99612280a725daaeb615ad7da06d05df43b49915
Andy Wang <andyatcal@berkeley.edu>
2015-07-28 16:16:45 -0700

    Changed math lib to glm

--- Circle.h
+++ Circle.h
@@ -6,16 +6,19 @@ class Circle{
 public:
 	Vector3f center;
 	float radius;
+	Vector3f normal;
 	Circle();
-	Circle(Vector3f center, float radius);
+	Circle(Vector3f center, float radius, Vector3f normal);
 };
 
 Circle::Circle(){
 	center = Vector3f(0, 0, 0);
 	radius = 0.0f;
+	normal = Vector3f(0, 0, 0);
 }
 
-Circle::Circle(Vector3f center, float radius){
+Circle::Circle(Vector3f center, float radius, Vector3f normal){
 	this -> center = center;
 	this -> radius = radius;
+	this -> normal = normal;
 }
