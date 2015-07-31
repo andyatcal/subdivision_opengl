749851674bd1c660de395f6f3f7000077e1e464b
Andy Wang <andyatcal@berkeley.edu>
2015-07-31 10:56:52 -0700

    Start of July 31

--- Circle.h
+++ Circle.h
@@ -4,21 +4,21 @@ using namespace glm;
 
 class Circle{
 public:
-	vec3 center;
-	float radius;
-	vec3 normal;
-	Circle();
-	Circle(vec3 center, float radius, vec3 normal);
+    vec3 center;
+    float radius;
+    vec3 normal;
+    Circle();
+    Circle(vec3 center, float radius, vec3 normal);
 };
 
 Circle::Circle(){
-	center = vec3(0, 0, 0);
-	radius = 0.0f;
-	normal = vec3(0, 0, 0);
+    center = vec3(0, 0, 0);
+    radius = 0.0f;
+    normal = vec3(0, 0, 0);
 }
 
 Circle::Circle(vec3 center, float radius, vec3 normal){
-	this -> center = center;
-	this -> radius = radius;
-	this -> normal = normal;
+    this -> center = center;
+    this -> radius = radius;
+    this -> normal = normal;
 }
